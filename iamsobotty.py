eeb57bd5e72ba3e973307c7f8bfb3af98a241e80
cthulhuplus <brookswm@gmail.com>
2018-03-12 09:39:27 -0400

    Updating iamsobotty.py

--- iamsobotty.py
+++ iamsobotty.py
@@ -2,10 +2,10 @@ import tweepy, time, sys
 
 argfile = str(sys.argv[1])
 
-CONSUMER_KEY = 'y9YcMQ0LoPJ27wqUZrtmrwbCw'
-CONSUMER_SECRET = 'teNYTvUbYXd8QojtkdB2tJBkfoXbEJS6mPpHxDDRonOw4DKzMf'
-ACCESS_KEY = '965609423644364801-dsbfLeIwy54ah3aN3ZL4VybftXOWe5e'
-ACCESS_SECRET = '5abLklwA5GPhlIH4rTsCZN8UCOfhJ4LF0sXGdC7DydauM'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
