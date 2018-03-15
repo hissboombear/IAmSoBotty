f34cf5d374e0158650162fcd5b0101d599370cbc
Kyle Felter <felterkyle94@gmail.com>
2018-03-15 12:25:40 -0400

    Added prefix and postfix to tweets

--- iamsobotty.py
+++ iamsobotty.py
@@ -10,10 +10,15 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+#Adds a prefix to all tweets
+prefix = 'Boop Beep\n'
+#Adds a postfix to all tweets
+postfix = '\nBeep Boop'
+
 filename=open(argfile, 'r')
 f=filename.readlines()
 filename.close()
 
 for line in f:
-    api.update_status(line)
+    api.update_status(prefix + line + postfix)
     time.sleep(900)
